d2eb012c0f752ecb9ec65ec92f6c24ef208d4f97
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-03 15:47:43 +0300

    localizable key added, reachability added on SplashScreen

--- Loodos/Screens/SplashScreen/ViewController/SplashScreenViewController.swift
+++ Loodos/Screens/SplashScreen/ViewController/SplashScreenViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class SplashScreenViewController: UIViewController {
+    
+    // MARK: - Constant
+    
+    private let connectionMessageLocalizationKey = "splashScreen.connectionMessage"
+    private let errorTitleLocalizationKey = "messageErrorTitle"
 
+    
+    // MARK: - Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if !Reachability.isConnectedToNetwork() {
+            self.createAlert(message: self.localizableGetString(forkey: connectionMessageLocalizationKey), title: self.localizableGetString(forkey: errorTitleLocalizationKey))
+            return
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
