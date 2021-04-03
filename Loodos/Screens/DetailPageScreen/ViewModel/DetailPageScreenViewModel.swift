f07fdc06128dc9492ea56703b2713e533be9dd7f
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-03 22:30:15 +0300

    firebaseAnalytics added on DetailPageScreenViewModel.

--- Loodos/Screens/DetailPageScreen/ViewModel/DetailPageScreenViewModel.swift
+++ Loodos/Screens/DetailPageScreen/ViewModel/DetailPageScreenViewModel.swift
@@ -6,8 +6,20 @@
 //
 
 import Foundation
+import FirebaseAnalytics
 
 class DetailPageScreenViewModel: NSObject {
     
+    // MARK: - Constans
     
+    private let eventName: String = "DetailScreen"
+    
+    override init() {
+        super.init()
+    }
+    
+    func firebaseAnalytics(detailData: SearchModel?) {
+        let parameter = ["title": detailData?.title, "year": detailData?.year, "imdb": detailData?.imdbID, "type": detailData?.type]
+        Analytics.logEvent(self.eventName, parameters: parameter as [String : Any])
+    }
 }
