fa874651eaa64be5490d1f70886a081e16cfb430
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-03 17:38:34 +0300

    Home Page viewmodel and protocol created.

---
+++ Loodos/Screens/HomePageScreen/Protocol/HomePageViewModelDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  HomePageViewModelDelegate.swift
+//  Loodos
+//
+//  Created by ENES AKSOY on 3.04.2021.
+//
+
+import Foundation
---
+++ Loodos/Screens/HomePageScreen/ViewModel/HomePageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomePageViewModel.swift
+//  Loodos
+//
+//  Created by ENES AKSOY on 3.04.2021.
+//
+
+import Foundation
+
+class HomePageViewModel: NSObject {
+    
+    
+}
