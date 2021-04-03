425d575d075220c3afd198729217f23e50c6c21f
Enes Aksoy <enesaksoy1168@gmail.com>
2021-04-03 16:08:08 +0300

    remoteConfigMethod added on SplashScreenViewModel

--- Loodos/Screens/SplashScreen/ViewModel/SplashScreenViewModel.swift
+++ Loodos/Screens/SplashScreen/ViewModel/SplashScreenViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import FirebaseRemoteConfig
+
+class SplashScreenViewModel: NSObject {
+    
+    // MARK: - Constans
+    
+    private let remoteConfigKey = "loodos_text"
+    private let remoteConfigMinimumFetchInterval: TimeInterval = 0
+    private let remoteConfigwithExpirationDuration: TimeInterval = 0
+    
+    
+    // MARK: - Properties
+    
+    private let remoteConfig = RemoteConfig.remoteConfig()
+    
+    // MARK: - Delegate
+    
+    var delegate: SplashScreenViewModelDelegate?
+    
+    // MARK: - View Model Methods
+    
+    override init() {
+        super.init()
+        remoteConfigMethod()
+    }
+    
+    func remoteConfigMethod() {
+        let defaults: [String: NSObject] = [self.remoteConfigKey: "" as NSObject]
+        remoteConfig.setDefaults(defaults)
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = remoteConfigMinimumFetchInterval
+        remoteConfig.configSettings = settings
+        
+        self.remoteConfig.fetch(withExpirationDuration: remoteConfigwithExpirationDuration) { [weak self] status, error in
+            if status == .success, error == nil {
+                self?.remoteConfig.activate { (isSucces, error) in
+                    guard error == nil else {
+                        return
+                    }
+                    let value = self?.remoteConfig.configValue(forKey: self?.remoteConfigKey).stringValue
+                    self?.delegate?.updateView(value ?? "")
+                }
+            } else {
+                print("error")
+            }
+        }
+    }
+}
